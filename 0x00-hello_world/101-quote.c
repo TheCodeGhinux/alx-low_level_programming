737f8c089267956930c0bdb39cab321a6181ded9
Ayomide <ilesanmibry@gmail.com>
2023-02-16 14:29:50 -0800

    commit 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <nistd.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ *main - Entry point
+ *Return: Always 1
  */
+
 int main(void)
 {
-        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdot\");
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
