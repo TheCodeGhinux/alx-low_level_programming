e2ff1bb864adef7d1add3d61e3037930635c3ab8
Ayomide <ilesanmibry@gmail.com>
2023-02-21 19:50:27 -0800

    feat: added new feature

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char *gbemi = "_putchar";
-
-	while (*gbemi)
-	{
-		_putchar(*gbemi);
-		gbemi++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
