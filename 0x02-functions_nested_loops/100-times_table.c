2e0e3d3dcc2372c6d7feb17fb3075e4408a7c027
Ayomide <ilesanmibry@gmail.com>
2023-02-21 20:41:05 -0800

    feat: added new feature

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - a function that prints all natural numbers from n to 98
- * user input's number prints to 98, regardless < 98 or > 98
- * @n: number input
+ * print_times_table - Entry point
+ * @n: input
  * Return: Always 0 (Success)
  */
-void print_to_98(int n)
+void print_times_table(int n)
 {
-	if (n < 98)
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
-	}
-	else
-	{
-		printf("98");
-	}
-	printf("\n");
+
+	return (0);
 }
