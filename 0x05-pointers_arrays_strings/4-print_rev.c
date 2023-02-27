cd02762707de885131cb87bed8de752610e41beb
CodeGhinux <ilesanmibry@gmail.com>
2023-02-27 13:06:56 -0800

    fix: fixed eror on code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_rev - Prints a string in reverse.
- * @s: string to be reserved.
+ * print_rev - print a string in reverse
+ * @s: char array string type
  */
+
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i;
 
-	while (s[i++])
-		len++;
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
-	for (i = len - 1; i >= 0; i--)
+	for (i--; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
