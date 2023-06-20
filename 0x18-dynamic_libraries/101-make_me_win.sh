a980de6c0809a75b66ec5335410b2470638f5d08
Ayomide <ilesanmibry@gmail.com>
2023-06-19 22:03:40 -0700

    commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/thecodeghinux/alx-low_level_programming/raw/main/0x18-dynamic_libraries/w.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -O /tmp/w.so /tmp https://github.com/thecodeghinux/alx-low_level_programming/raw/main/0x18-dynamic_libraries/w.so
+export LD_PRELOAD=/tmp/w.so
