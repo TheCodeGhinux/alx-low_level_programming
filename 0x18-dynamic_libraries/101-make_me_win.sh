8754a9c956e976b73e6206aee5bb9e806df6d119
Ayomide <ilesanmibry@gmail.com>
2023-06-20 20:45:41 -0700

    commit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/w.so https://github.com/thecodeghinux/alx-low_level_programming/raw/main/0x18-dynamic_libraries/w.so
+wget -P /tmp https://github.com/thecodeghinux/alx-low_level_programming/raw/main/0x18-dynamic_libraries/w.so
 export LD_PRELOAD=/tmp/w.so
